65bbda19568b21ffb8b71a3b5878e13fe4518b66
Yacob Kazal <yacob.kazal@westpac.com.au>
2021-09-06 07:48:59 +1000

    Initial Commit

---
+++ AirportsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirportsList
+//
+//  Created by Yacob Kazal on 6/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
